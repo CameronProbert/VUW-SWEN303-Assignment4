4292aab612053635ce66687956c749b0288b6c1b
Francine Burling-Claridge <francinebc.mail@gmail.com>
2015-06-14 17:30:53 +1200

    more work on the index.html

--- Pages/index.js
+++ Pages/index.js
@@ -7,43 +7,48 @@ var colorBlack = "rgb(0, 0, 0)";
 
 // GLOBAL SIZES
 var svgWidth = 1000;
-var svgHeight = 450;
+var svgHeight = 550;
 var barWidth = 100;
 var arrowSize = 50;
 
 // GLOBAL PADDINGS
 var padding = 100;
 var barPadding = 5;
-var bottomPadding = 50;
+var bottomPadding = 75;
 
 
-var year = 2013;
+var year = 2008;
 doPage(year);
+
+
+
 
 function doPage (year) {
 d3.csv('data/'+year+'-Table1.csv', function (error, data){
 	if(error){return;}
 	// TODO neeeeeeeeeed to clear the SVG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	clearSVG(svg);
 	for(var i=0; i<data.length; i++){
-		console.log(data[i]);
+		//console.log(data[i]);
 	}
 	var rectHeights = [100, 200, 150];
 	var svg = getNewSVG(svgWidth, svgHeight);
-	svg.selectAll("rect") // background
+	svg.selectAll("rect") // podiums
         .data(rectHeights)
         .enter()
         .append("rect")
         .attr("x", function(d, i){
-			return i*(svgWidth-padding*2)/3+padding;
+			return i * ( svgWidth - padding * 2 ) / 3 + padding;
 		})
         .attr("y", function(d){
-			return svgHeight-d;//+padding;
+			return svgHeight - d - bottomPadding;
 		})
         .attr("width",(svgWidth-padding*2)/3-barPadding)
         .attr("height", function(d){
 			return d;
 		})
         .attr("fill", colorPink);
+		
 	var topThree = getPlacings(data);
 	var imgs = svg.selectAll("image")
 		.data(topThree)
@@ -56,17 +61,17 @@ d3.csv('data/'+year+'-Table1.csv', function (error, data){
 			return i*(svgWidth-padding*2)/3+padding + (svgWidth-padding*2)/12 - 2*barPadding;
 		})
         .attr("y", function(d, i){
-			return svgHeight-rectHeights[i]-(svgWidth-padding*2)/6;//+padding;
+			return svgHeight-rectHeights[i]-(svgWidth-padding*2)/6 - bottomPadding;
 		})
         .attr("width",(svgWidth-padding*2)/6)
         .attr("height",(svgWidth-padding*2)/6);
 	if(year===2013){
-		drawLeftTri(svg);
+		drawLeftTri(svg, year);
 	}else if(year===2008){
-		drawRightTri(svg);
+		drawRightTri(svg, year);
 	}else{
-		drawLeftTri(svg);
-		drawRightTri(svg);
+		drawLeftTri(svg, year);
+		drawRightTri(svg, year);
 	}
 	
 });
@@ -79,26 +84,49 @@ function getNewSVG (w, h) {
 	return svg;
 }
 
-function drawLeftTri (svg)  {
+function clearSVG(svg){
+	d3.selectAll("svg > *").remove();
+	//console.log("tried to clear the svg");
+}
+
+function drawLeftTri (svg, year)  {
+	var tip = "Previous Year";
 	svg.append("svg:image")
 		.attr("xlink:href", function(d){
 			return "../Resources/LeftArrow.png";
 		})
-		.attr("x", padding/2)
-        .attr("y", svgHeight - bottomPadding/2)
+		.attr("x", 0)
+        .attr("y", svgHeight - arrowSize)
         .attr("width", arrowSize)
-        .attr("height", arrowSize);
+        .attr("height", arrowSize)
+		//.append("svg:title")
+		//.text(tip)
+		.on('click', function(){
+			//console.log("I was clicked");
+			d3.event.stopPropagation();
+			year = year - 1;
+			doPage(year);
+		});
 }
 
-function drawRightTri (svg)  {
+function drawRightTri (svg, year)  {
+	var tip = "Next Year";
 	svg.append("svg:image")
 		.attr("xlink:href", function(d){
 			return "../Resources/RightArrow.png";
 		})
-		.attr("x", svgWidth - padding/2)
-        .attr("y", svgHeight - bottomPadding/2)
+		.attr("x", svgWidth - arrowSize )
+        .attr("y", svgHeight - arrowSize)
         .attr("width", arrowSize)
-        .attr("height", arrowSize);
+        .attr("height", arrowSize)
+		//.append("svg:title")
+		//.text(tip)
+		.on('click', function(){
+			//console.log("I was clicked");
+			d3.event.stopPropagation();
+			year = year + 1;
+			doPage(year);
+		});
 }
 
 function getPlacings (data) {
@@ -106,7 +134,9 @@ function getPlacings (data) {
 	var lastGame = data[data.length-1];
 	var penUltGame = data[data.length-2];
 	var score = lastGame.Score;
-	var res = score.split(" - ");
+	var res = score.split("-");
+	res[0] = doTrim(res[0]);
+	res[1] = doTrim(res[1]);
 	if(res[0]>res[1]){
 		topThree[0] = lastGame['Home Team'];
 		topThree[1] = lastGame['Away Team'];
@@ -115,7 +145,9 @@ function getPlacings (data) {
 		topThree[0] = lastGame['Away Team'];
 	}
 	score = penUltGame.Score;
-	var res = score.split(" - ");
+	var res = score.split("-");
+	res[0] = doTrim(res[0]);
+	res[1] = doTrim(res[1]);
 	if(res[0]>res[1]){
 		topThree[2] = penUltGame['Away Team'];
 	}else{
@@ -132,5 +164,19 @@ function ordered (topThree) {
 	return placings;
 }
 
+function doTrim(str){
+	var fin;
+	if(str.charAt(0)==" "){
+		var finPos = str.length-1;
+		fin = str.substring(1, finPos);
+	}else{
+		fin = str;
+	}
+	var finPos = fin.length-1;
+	if(fin.charAt(finPos)==" "){
+		fin = fin.substring(0, finPos-1);
+	}
+	return fin;
+}
 
 }
