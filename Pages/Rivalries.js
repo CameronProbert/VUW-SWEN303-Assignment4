ba9ce48e856f6cae97cc0b0431311ed8f6954ec2
Cameron Probert <c.probert@hotmail.co.nz>
2015-06-19 19:18:36 +1200

    Added expanding banners

--- Pages/Rivalries.js
+++ Pages/Rivalries.js
@@ -1,22 +1,29 @@
 // COLOURS
-var colorDarkBlue = "rgb(0, 122,182)";
-var colorLightBlue = "rgb(0, 157, 221)";
-var colorWhite = "rgb(255, 255, 255)";
-var colorPink = "rgb(237, 0, 140)";
-var colorBlack = "rgb(0, 0, 0)";
+var colourDarkBlue = "rgb(0, 122,182)";
+var colourLightBlue = "rgb(0, 157, 221)";
+var colourWhite = "rgb(255, 255, 255)";
+var colourPink = "rgb(237, 0, 140)";
+var colourBlack = "rgb(0, 0, 0)";
+var colourGrey = "rgb(220, 220, 220)";
 
 // GLOBAL SIZES
 var svgWidth = 1000;
-var svgHeight = 550;
+var svgHeight = 1000;
+var iconSize = 100;
+var rivalryPadding = 30;
 var barWidth = 100;
 var arrowSize = 50;
 
 // GLOBAL PADDINGS
-var padding = 100;
+var padding = 50;
 var barPadding = 5;
 var bottomPadding = 75;
 
-//doPage();
+var RIVALRIES_TO_SHOW = 6;
+
+var selectedRivalry = 0;//RIVALRIES_TO_SHOW+1;
+
+doPage();
 
 function doPage () {
 	// Read in each year's data into data<year>
@@ -48,30 +55,21 @@ function doPage () {
 										// Find if the teams are from this matchup
 										if (matchups[i].team1 === allData[countData][countEntry]['Home Team']){
 											if (matchups[i].team2 === allData[countData][countEntry]['Away Team']){
-												// This matchup so add one to the number of games played
+												// Was this matchup so add one to the number of games played
 												matchups[i].games++;
 
 												// Check to see if team1 won
-												var score = allData[countData][countEntry].Score;
-												console.log(score);
-												var res = score.split(/-|–/);
-												res[0] = doTrim(res[0]);
-												res[1] = doTrim(res[1]);
-												if (res[0] > res[1]){
+												if (allData[countData][countEntry]['Home Score'] > allData[countData][countEntry]['Away Score']){
 													matchups[i].won++;
 												}
 											}
 										} else if (matchups[i].team1 === allData[countData][countEntry]['Away Team']){
 											if (matchups[i].team2 === allData[countData][countEntry]['Home Team']){
-												// This matchup so add one to the number of games played
+												// Was this matchup so add one to the number of games played
 												matchups[i].games++;
 
 												// Check to see if team1 won
-												var score = allData[countData][countEntry].Score;
-												var res = score.split("-");
-												res[0] = doTrim(res[0]);
-												res[1] = doTrim(res[1]);
-												if (res[1] > res[0]){
+												if (allData[countData][countEntry]['Away Score'] > allData[countData][countEntry]['Home Score']){
 													matchups[i].won++;
 												}
 											}
@@ -83,24 +81,288 @@ function doPage () {
 							// Iterate through each matchup to determine the rivalries
 							var rivalries = [];
 							for (var i = 0; i < matchups.length; i++){
-							var gamesPlayed = matchups[i].games;
-							var gamesWon = matchups[i].won;
-								if (gamesWon/gamesPlayed >= 0.25 && gamesWon/gamesPlayed <= 0.75){
-									rivalries[rivalries.length] = matchups[i];
-								}
+								var gamesPlayed = matchups[i].games;
+								var gamesWon = matchups[i].won;
+									if (gamesWon/gamesPlayed >= 0.25 && gamesWon/gamesPlayed <= 0.75){
+										rivalries[rivalries.length] = matchups[i];
+									}
 							}
-							console.log(rivalries.length);
-
-							// Find the top 6 rivalries (closest to 50% wins each team in a matchup)
-							for (var i = 0; i < rivalries.length; i++){
-
+							
+							// Sort the rivalries by closest to 50%
+							rivalries.sort(function(a,b){
+							
+								// Find winrate for each team
+								var winRateA = a.won/a.games;
+								var winRateB = b.won/b.games;
+								
+								// Find the absolute difference from 50%
+								var diffA = Math.abs(winRateA - 0.5);
+								var diffB = Math.abs(winRateB - 0.5);
+								
+								return diffA-diffB;
+							});
+							
+							var topRivalries = [];
+							// Obtain the number of rivalries specified in RIVALRIES_TO_SHOW
+							for (var i = 0; i < RIVALRIES_TO_SHOW && i < rivalries.length; i++){
+								topRivalries[topRivalries.length] = rivalries[i];
 							}
+							
+							drawRivalries(svg, topRivalries, selectedRivalry);
+							
 						});
 					});
 				});
 			});
 		});
 	});
+}
+
+function drawRivalries(svg, rivalries, selectedRivalry){
+
+	// Loop through all the rivalries
+	for (var count = 0; count < RIVALRIES_TO_SHOW && count < rivalries.length; count++){
+	
+		// If this is the selected rivalry
+		if (count === selectedRivalry){
+			drawSelectedRivalry(count, svg, rivalries);
+		}
+		// If this is not the selected rivalry
+		else {
+			drawUnselectedRivalry(count, selectedRivalry, svg, rivalries);
+		}
+	}
+}
+
+// Draws the selected rivalry
+function drawSelectedRivalry(count, svg, rivalries){
+
+	// Text area variables
+	var infoX = 220;
+	var infoY = count*(iconSize+rivalryPadding)+iconSize/2;
+	var infoWidth = 560;
+	var infoHeight = 300;
+	var textSize = 60;
+	var textPadding = 10;	
+	
+	// Display text box
+	svg.append("rect")
+		.attr("x", infoX)
+		.attr("y", infoY)
+		.attr("fill", colourGrey)
+		.attr("stroke", colourBlack)
+		.attr("width", infoWidth)
+		.attr("height", infoHeight);
+		
+	// Do box
+		
+	// Display team on left
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/logos/logo_" + rivalries[count].team1 + ".png";
+		})
+		.attr("x", 100)
+		.attr("y", function(){
+			return count*(iconSize+rivalryPadding);
+		})
+		.attr("width",iconSize)
+		.attr("height",iconSize)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+	
+	// Display team on right
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/logos/logo_" + rivalries[count].team2 + ".png";
+		})
+		.attr("x", 800)
+		.attr("y", function(){
+			return count*(iconSize+rivalryPadding);
+		})
+		.attr("width",iconSize)
+		.attr("height",iconSize)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+
+	var bannerWidth = iconSize*3;
+	var bannerHeight = iconSize;
+	var bannerPadding = 10;
+
+	// Display team banners for left teams
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/banners/banner_" + rivalries[count].team1 + ".png";
+		})
+		.attr("x", 210)
+		.attr("y", function(){
+			return count*(iconSize+rivalryPadding);
+		})
+		.attr("width",bannerWidth)
+		.attr("height",bannerHeight)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+
+	// Display team banners for right teams
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/banners/banner_" + rivalries[count].team2 + ".png";
+		})
+		.attr("x", 800-bannerWidth-bannerPadding)
+		.attr("y", function(){
+			return count*(bannerHeight+rivalryPadding);
+		})
+		.attr("width",bannerWidth)
+		.attr("height",bannerHeight)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+
+	// Display versus symbol in the centre of each banner
+	var vsSize = 150;
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/banners/banner_" + rivalries[count].team2 + ".png";
+		})
+		.attr("x", 800-bannerWidth-bannerPadding)
+		.attr("y", function(){
+			return count*(bannerHeight+rivalryPadding);
+		})
+		.attr("width",bannerWidth)
+		.attr("height",bannerHeight)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+
+	// Display versus symbol in the centre of each banner
+	var vsSize = 100;
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/banners/banner_VS.png";
+		})
+		.attr("x", 1/2*svgWidth-1/2*vsSize)
+		.attr("y", function(){
+			return count*(bannerHeight+rivalryPadding);
+		})
+		.attr("width",vsSize)
+		.attr("height",vsSize)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+}
+
+// Draws a single unselected rivalry
+function drawUnselectedRivalry(count, selectedRivalry, svg, rivalries){
+	console.log("count: " + count + "| selected: " + selectedRivalry );
+	var modifier = 0;
+	if (count > selectedRivalry){
+		modifier = 250;
+		console.log("Adjusting Modifier");
+	}
+
+	// Display team on left
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/logos/logo_" + rivalries[count].team1 + ".png";
+		})
+		.attr("x", 100)
+		.attr("y", function(){
+			
+			return count*(iconSize+rivalryPadding)+modifier;
+		})
+		.attr("width",iconSize)
+		.attr("height",iconSize)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+	
+	// Display team on right
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/logos/logo_" + rivalries[count].team2 + ".png";
+		})
+		.attr("x", 800)
+		.attr("y", function(){
+			return count*(iconSize+rivalryPadding)+modifier;
+		})
+		.attr("width",iconSize)
+		.attr("height",iconSize)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+
+	var bannerWidth = iconSize*3;
+	var bannerHeight = iconSize;
+	var bannerPadding = 10;
+
+	// Display team banners for left teams
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/banners/banner_" + rivalries[count].team1 + ".png";
+		})
+		.attr("x", 210)
+		.attr("y", function(){
+			return count*(iconSize+rivalryPadding)+modifier;
+		})
+		.attr("width",bannerWidth)
+		.attr("height",bannerHeight)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+
+	// Display team banners for right teams
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/banners/banner_" + rivalries[count].team2 + ".png";
+		})
+		.attr("x", 800-bannerWidth-bannerPadding)
+		.attr("y", function(){
+			return count*(bannerHeight+rivalryPadding)+modifier;
+		})
+		.attr("width",bannerWidth)
+		.attr("height",bannerHeight)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+
+	// Display versus symbol in the centre of each banner
+	var vsSize = 100;
+	svg.append("svg:image")
+		.attr("xlink:href", function(){
+			return "../Resources/banners/banner_VS.png";
+		})
+		.attr("x", 1/2*svgWidth-1/2*vsSize)
+		.attr("y", function(){
+			return count*(bannerHeight+rivalryPadding)+modifier;
+		})
+		.attr("width",vsSize)
+		.attr("height",vsSize)
+		.on("click",function(){
+			selectRivalry(count);
+			doPage();
+		});
+}
+
+// Selects the given rivalry, if it is already selected then change it to false
+function selectRivalry(index){
+	if (selectedRivalry === index){
+		selectedRivalry = RIVALRIES_TO_SHOW+1;
+	}
+	else {
+	selectedRivalry = index;
+	}
 }
 
 // Creates every combination of team matchups
@@ -117,18 +379,15 @@ function createRivalries(){
 				team1:allTeams[teamCount],
 				team2:allTeams[oppTeamCount],
 				games:0,
-				wins:0
+				won:0
 			};
-			//matchups[numRivalries].team1 = allTeams[teamCount];
-			//matchups[numRivalries].team2 = allTeams[oppTeamCount];
-			//matchups[numRivalries].games = 0;
-			//matchups[numRivalries].wins = 0;
 			numRivalries++;
 		}
 	}
 	return matchups;
 }
 
+// Creates an svg with the given width and height
 function getNewSVG (w, h) {
     var svg = d3.select("#mainsvg")
         .attr("width", w)
@@ -137,6 +396,7 @@ function getNewSVG (w, h) {
 	return svg;
 }
 
+// Removes all items from all svgs
 function clearSVG () {
 	d3.selectAll("svg > *").remove();
 }
